39a87966b2ae96ad238837d7649713ccaed6042d
Yagyaraz <yagyaraz28@gmail.com>
2025-10-27 01:43:44 +0545

    landingpage started

---
+++ AgricultureView/Areas/Admin/Models/LandingPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AgricultureView.Areas.Admin.Models
+{
+    public class LandingPageViewModel
+    {
+        public List<FarmerViewModel> Farmers { get; set; }
+        public List<AgricultureProgramViewModel>Programs { get; set; }
+        public List<AgricultureProjectViewModel> Projects { get; set; }
+        public List<AgricultureServiceViewModel> Services { get; set; }
+        public List<AgricultureServiceAdditionalViewModel> AdditionalServices { get; set; }
+
+
+    }
+}
--- AgricultureView/Controllers/LandingPageController.cs
+++ AgricultureView/Controllers/LandingPageController.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AgricultureView.Areas.Admin.Models;
+using AgricultureView.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AgricultureView.Controllers
 {
        [AllowAnonymous]
     public class LandingPageController : Controller
     {
-        public IActionResult Index()
+        private readonly IGlobalVeriable _globalVeriable;
+        public LandingPageController(IGlobalVeriable globalVeriable)
         {
-            return View();
+            _globalVeriable = globalVeriable;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new LandingPageViewModel();
+            var programResponse = await _globalVeriable.GetMethod("Admin/AgriculturePlan/GetAllProgram");
+            if (programResponse.Status)
+            {
+                model.Programs = JsonConvert.DeserializeObject<List<AgricultureProgramViewModel>>(programResponse.Data.ToString());
+            }
+            var projectResponse = await _globalVeriable.GetMethod("Admin/AgriculturePlan/GetAllProject");
+            if (projectResponse.Status)
+            {
+                model.Projects = JsonConvert.DeserializeObject<List<AgricultureProjectViewModel>>(projectResponse.Data.ToString());
+            }
+            var serviceResponse = await _globalVeriable.GetMethod("Admin/AgriculturePlan/GetAllService");
+            if (serviceResponse.Status)
+            {
+              model.Services = JsonConvert.DeserializeObject<List<AgricultureServiceViewModel>>(serviceResponse.Data.ToString());
+            }
+            var farmerRsponse = await _globalVeriable.GetMethod("Admin/Farmer/GetAllFarmer");
+            if (farmerRsponse.Status)
+            {
+                model.Farmers = JsonConvert.DeserializeObject<List<FarmerViewModel>>(farmerRsponse.Data.ToString());
+            }
+            //var formResponse = await _globalVeriable.GetMethod("Admin/AgriculturePlan/GetAllApplicatoionForm");
+            //if (formResponse.Status)
+            //{
+            //    model.AdditionalServices = JsonConvert.DeserializeObject<List<AgricultureApplicatoionFormViewModel>>(formResponse.Data.ToString());
+            //}
+            return View(model);
         }
     }
 }
