f681bc5b183cc45645171ba3601ac1e25f1bacc8
Yagyaraz <yagyaraz28@gmail.com>
2025-10-29 20:15:16 +0545

    all landing page

--- AgricultureView/Areas/Admin/Models/LandingPageViewModel.cs
+++ AgricultureView/Areas/Admin/Models/LandingPageViewModel.cs
@@ -7,7 +7,31 @@
         public List<AgricultureProjectViewModel> Projects { get; set; }
         public List<AgricultureServiceViewModel> Services { get; set; }
         public List<AgricultureServiceAdditionalViewModel> AdditionalServices { get; set; }
-
+        public List<AgricultureFarmerGroupViewModel> FarmerGroups { get; set; }
+        public List<AgriCalendarTypeViewModel> CalenderTypes { get; set; }
+        public List<AgriCalendarTypeViewModel> AgriCalendarTypes { get; set; }
+        public List<AgriCalendarCategoryViewModel> AgriCalendarCategories { get; set; }
+        public List<AgriCalendarProductViewModel> AgriCalendarProducts { get; set; }
+        public List<AgriCalendarViewModel> AgriCalendars { get; set; }
+        public List<FarmerServiceViewModel> FarmerServices { get; set; }
+        public List<FarmerServiceCardViewModel> FarmerServiceCards { get; set; }
+        public List<AlbumViewModel> Albums { get; set; }
+        public List<ImageGalleryViewModel> ImageGalleries { get; set; }
+        public List<LibraryViewModel> Libraries { get; set; }
+        public List<CommonViewModel> Markets { get; set; }
+        public List<MarketPriceViewModel> MarketPrices { get; set; }
+        public List<FertilizerStoreViewModel> FertilizerStores { get; set; }
+        public List<SeedStoreViewModel> SeedStores { get; set; }
+        public List<InsuranceCenterViewModel> InsuranceCenters { get; set; }
+        public List<AgricultureEquipmentViewModel> AgricultureEquipments { get; set; }
+        public List<CategoryViewModel> Categories { get; set; }
+        public List<SubCategoryViewModel> SubCategories { get; set; }
+        public List<SubsidyViewModel> SubsidiesForUser { get; set; }
+        public List<SubsidyViewModel> Subsidies { get; set; }
+        public List<OtherSubsidyViewModel> OtherSubsidies { get; set; }
+        public List<TrainingViewModel> Trainings { get; set; }
+        public List<PlaylistViewModel> Playlists { get; set; }
+        public List<VideoGalleryViewModel> VideoGalleries { get; set; }
 
     }
 }
--- AgricultureView/Controllers/LandingPageController.cs
+++ AgricultureView/Controllers/LandingPageController.cs
@@ -38,11 +38,137 @@ namespace AgricultureView.Controllers
             {
                 model.Farmers = JsonConvert.DeserializeObject<List<FarmerViewModel>>(farmerRsponse.Data.ToString());
             }
-            //var formResponse = await _globalVeriable.GetMethod("Admin/AgriculturePlan/GetAllApplicatoionForm");
-            //if (formResponse.Status)
-            //{
-            //    model.AdditionalServices = JsonConvert.DeserializeObject<List<AgricultureApplicatoionFormViewModel>>(formResponse.Data.ToString());
-            //}
+            var agriCalendarTypeResponse = await _globalVeriable.GetMethod("Admin/AgriCalendar/GetAllAgriCalendarType");
+            if (agriCalendarTypeResponse.Status)
+            {
+                model.AgriCalendarTypes = JsonConvert.DeserializeObject<List<AgriCalendarTypeViewModel>>(agriCalendarTypeResponse.Data.ToString());
+            }
+            var agriCalendarCategoryResponse = await _globalVeriable.GetMethod("Admin/AgriCalendar/GetAllAgriCalendarCategory");
+            if (agriCalendarCategoryResponse.Status)
+            {
+                model.AgriCalendarCategories = JsonConvert.DeserializeObject<List<AgriCalendarCategoryViewModel>>(agriCalendarCategoryResponse.Data.ToString());
+            }
+
+            var agriCalendarProductResponse = await _globalVeriable.GetMethod("Admin/AgriCalendar/GetAllAgriCalendarProduct");
+            if (agriCalendarProductResponse.Status)
+            {
+                model.AgriCalendarProducts = JsonConvert.DeserializeObject<List<AgriCalendarProductViewModel>>(agriCalendarProductResponse.Data.ToString());
+            }
+
+            var agriCalendarResponse = await _globalVeriable.GetMethod("Admin/AgriCalendar/GetAllAgriCalendar");
+            if (agriCalendarResponse.Status)
+            {
+                model.AgriCalendars = JsonConvert.DeserializeObject<List<AgriCalendarViewModel>>(agriCalendarResponse.Data.ToString());
+            }
+            var farmerServiceResponse = await _globalVeriable.GetMethod("Admin/FarmerService/GetAllFarmerService");
+            if (farmerServiceResponse.Status)
+            {
+                model.FarmerServices = JsonConvert.DeserializeObject<List<FarmerServiceViewModel>>(farmerServiceResponse.Data.ToString());
+            }
+
+            var farmerServiceCardResponse = await _globalVeriable.GetMethod("Admin/FarmerService/GetAllFarmerServiceCard");
+            if (farmerServiceCardResponse.Status)
+            {
+                model.FarmerServiceCards = JsonConvert.DeserializeObject<List<FarmerServiceCardViewModel>>(farmerServiceCardResponse.Data.ToString());
+            }
+            var albumResponse = await _globalVeriable.GetMethod("Admin/ImageGallery/GetAllAlbum");
+            if (albumResponse.Status)
+            {
+                model.Albums = JsonConvert.DeserializeObject<List<AlbumViewModel>>(albumResponse.Data.ToString());
+            }
+
+            var imageGalleryResponse = await _globalVeriable.GetMethod("Admin/ImageGallery/GetAllImageGallery");
+            if (imageGalleryResponse.Status)
+            {
+                model.ImageGalleries = JsonConvert.DeserializeObject<List<ImageGalleryViewModel>>(imageGalleryResponse.Data.ToString());
+            }
+            var libraryResponse = await _globalVeriable.GetMethod("Admin/Library/GetAllLibrary");
+            if (libraryResponse.Status)
+            {
+                model.Libraries = JsonConvert.DeserializeObject<List<LibraryViewModel>>(libraryResponse.Data.ToString());
+            }
+
+            var marketResponse = await _globalVeriable.GetMethod("Admin/MarketPrice/GetAllMarket");
+            if (marketResponse.Status)
+            {
+                model.Markets = JsonConvert.DeserializeObject<List<CommonViewModel>>(marketResponse.Data.ToString());
+            }
+
+            var marketPriceResponse = await _globalVeriable.GetMethod("Admin/MarketPrice/GetAllMarketPrice");
+            if (marketPriceResponse.Status)
+            {
+                model.MarketPrices = JsonConvert.DeserializeObject<List<MarketPriceViewModel>>(marketPriceResponse.Data.ToString());
+            }
+            var fertilizerStoreResponse = await _globalVeriable.GetMethod("Admin/StoresCenter/GetAllFertilizerStore");
+            if (fertilizerStoreResponse.Status)
+            {
+                model.FertilizerStores = JsonConvert.DeserializeObject<List<FertilizerStoreViewModel>>(fertilizerStoreResponse.Data.ToString());
+            }
+
+            var seedStoreResponse = await _globalVeriable.GetMethod("Admin/StoresCenter/GetAllSeedStore");
+            if (seedStoreResponse.Status)
+            {
+                model.SeedStores = JsonConvert.DeserializeObject<List<SeedStoreViewModel>>(seedStoreResponse.Data.ToString());
+            }
+
+            var insuranceCenterResponse = await _globalVeriable.GetMethod("Admin/StoresCenter/GetAllInsuranceCenter");
+            if (insuranceCenterResponse.Status)
+            {
+                model.InsuranceCenters = JsonConvert.DeserializeObject<List<InsuranceCenterViewModel>>(insuranceCenterResponse.Data.ToString());
+            }
+
+            var agricultureEquipmentResponse = await _globalVeriable.GetMethod("Admin/StoresCenter/GetAllAgricultureEquipment");
+            if (agricultureEquipmentResponse.Status)
+            {
+                model.AgricultureEquipments = JsonConvert.DeserializeObject<List<AgricultureEquipmentViewModel>>(agricultureEquipmentResponse.Data.ToString());
+            }
+            var categoryResponse = await _globalVeriable.GetMethod("Admin/Subsidy/GetAllCategory");
+            if (categoryResponse.Status)
+            {
+                model.Categories = JsonConvert.DeserializeObject<List<CategoryViewModel>>(categoryResponse.Data.ToString());
+            }
+
+            var subCategoryResponse = await _globalVeriable.GetMethod("Admin/Subsidy/GetAllSubCategory");
+            if (subCategoryResponse.Status)
+            {
+                model.SubCategories = JsonConvert.DeserializeObject<List<SubCategoryViewModel>>(subCategoryResponse.Data.ToString());
+            }
+
+            var subsidyForUserResponse = await _globalVeriable.GetMethod("Admin/Subsidy/GetAllSubsidyForUser");
+            if (subsidyForUserResponse.Status)
+            {
+                model.SubsidiesForUser = JsonConvert.DeserializeObject<List<SubsidyViewModel>>(subsidyForUserResponse.Data.ToString());
+            }
+
+            var subsidyResponse = await _globalVeriable.GetMethod("Admin/Subsidy/GetAllSubsidy");
+            if (subsidyResponse.Status)
+            {
+                model.Subsidies = JsonConvert.DeserializeObject<List<SubsidyViewModel>>(subsidyResponse.Data.ToString());
+            }
+
+            var otherSubsidyResponse = await _globalVeriable.GetMethod("Admin/Subsidy/GetAllOtherSubsidy");
+            if (otherSubsidyResponse.Status)
+            {
+                model.OtherSubsidies = JsonConvert.DeserializeObject<List<OtherSubsidyViewModel>>(otherSubsidyResponse.Data.ToString());
+            }
+            var trainingResponse = await _globalVeriable.GetMethod("Admin/Training/GetAllTraining");
+            if (trainingResponse.Status)
+            {
+                model.Trainings = JsonConvert.DeserializeObject<List<TrainingViewModel>>(trainingResponse.Data.ToString());
+            }
+
+            var playlistResponse = await _globalVeriable.GetMethod("Admin/VideoGallery/GetAllPlaylist");
+            if (playlistResponse.Status)
+            {
+                model.Playlists = JsonConvert.DeserializeObject<List<PlaylistViewModel>>(playlistResponse.Data.ToString());
+            }
+
+            var videoGalleryResponse = await _globalVeriable.GetMethod("Admin/VideoGallery/GetAllVideoGallery");
+            if (videoGalleryResponse.Status)
+            {
+                model.VideoGalleries = JsonConvert.DeserializeObject<List<VideoGalleryViewModel>>(videoGalleryResponse.Data.ToString());
+            }
+
             return View(model);
         }
     }
