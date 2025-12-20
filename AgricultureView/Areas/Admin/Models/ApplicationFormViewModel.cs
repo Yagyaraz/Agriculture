5d1519ee7e1345bc8bc36ab2f20f9449cb4f1359
Yagyaraz <yagyaraz28@gmail.com>
2025-12-21 00:13:47 +0545

    application form

---
+++ AgricultureView/Areas/Admin/Models/ApplicationFormViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgricultureView.Areas.Admin.Models
+{
+    public class ApplicationFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public string  Dob { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public string Program { get; set; }
+
+        [Required]
+        public string Batch { get; set; }
+
+        public string Qualification { get; set; }
+        public string Institution { get; set; }
+        public string Experience { get; set; }
+
+        [Required]
+        public IFormFile Resume { get; set; }
+        public string ResumePath {  get; set; }
+
+        public string Source { get; set; }
+        public string Message { get; set; }
+    }
+}
